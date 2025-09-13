4a2f5c7071987668f1935d356dcc70f9027f7ce7
iwanaga1 <k024g1001@g.neec.ac.jp>
2025-09-13 15:25:39 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	//初めは1970年の駅名リスト
+	list <const char*> eki_list{
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Sinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
+
+
+	printf("1970年\n");
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); it_f++) {
+		std::cout << *it_f << endl;
+	}
+
+	printf("\n2019年\n");
+	list<const char*>::iterator it_f;
+	for (auto it_f = eki_list.begin(); it_f != eki_list.end(); ++it_f) {
+		if (strcmp(*it_f, "Nippori") == 0) {
+			it_f = eki_list.insert(it_f, "Nishi-Nippori");
+			std::cout << *it_f << endl;
+			++it_f;
+		}
+		std::cout << *it_f << endl;
+	}
+
+	printf("\n2022年\n");
+	for (auto it_f = eki_list.begin(); it_f != eki_list.end(); ++it_f) {
+
+		if (strcmp(*it_f, "Sinagawa") == 0) {
+			it_f = eki_list.insert(it_f, "Takanawa Gateway");
+			std::cout << *it_f << endl;
+			++it_f;
+		}
+		std::cout << *it_f << endl;
+	}
+
+	return 0;
+}
